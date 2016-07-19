15375a1d03e160ff0df48cbf38a5d217f8327852
ron93 <ronmkare1@gmail.com>
2016-07-19 08:50:40 +0300

    edits

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(document).foundation()
-
+//fade header background color on scroll
 $(window).scroll(function(){
-    $("#top").css("opacity", 1 - $(window).scrollTop() / 250);
+  $("#top").css("opacity", 1 - $(window).scrollTop() / 250);
   });
+
+
+
+//onclick border form on nav 
+
+$('.box').on('click', function(e){
+      e.preventDefault();
+      $(this).toggleClass('clicky');
+    });
+
+
+
+
